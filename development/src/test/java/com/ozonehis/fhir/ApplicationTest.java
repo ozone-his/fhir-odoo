158d54aeee7c2ae4863a25aba0ebc88b05db8209
Kipchumba C. Bett <kbett68@gmail.com>
2024-09-03 15:26:44 +0300

    OZ-684: Set up development environment

---
+++ development/src/test/java/com/ozonehis/fhir/ApplicationTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2024, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.fhir;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    @DisplayName("Should load ApplicationContext")
+    void contextLoads() {
+        assertNotNull(context);
+    }
+}
