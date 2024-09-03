6a8ae1f7f34dc8d82eff7adb3539d78df9c3b1aa
Kipchumba C. Bett <kbett68@gmail.com>
2024-09-03 15:26:44 +0300

    OZ-684: Set up development environment

---
+++ development/src/main/java/com/ozonehis/fhir/Application.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2024, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.fhir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
