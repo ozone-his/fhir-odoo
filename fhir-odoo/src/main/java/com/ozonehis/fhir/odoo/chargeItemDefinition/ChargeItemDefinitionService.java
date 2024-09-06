a67ff32412774c71780c5f5de6963275c58c09be
Kipchumba C. Bett <kbett68@gmail.com>
2024-09-06 18:05:17 +0300

    OZ-693: Implement read & search for ChargeItemDefinition resource

---
+++ fhir-odoo/src/main/java/com/ozonehis/fhir/odoo/chargeItemDefinition/ChargeItemDefinitionService.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2024, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.fhir.odoo.chargeItemDefinition;
+
+import ca.uhn.fhir.rest.param.TokenAndListParam;
+import com.ozonehis.fhir.odoo.FhirService;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.ChargeItemDefinition;
+
+public interface ChargeItemDefinitionService extends FhirService<ChargeItemDefinition> {
+
+    Bundle searchForChargeItemDefinitions(TokenAndListParam code);
+}
