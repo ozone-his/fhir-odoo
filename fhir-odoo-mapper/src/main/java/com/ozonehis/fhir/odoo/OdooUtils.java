86654cb3d027f24f2ecf1fb052f372daef733b62
Siddharth Vaish <sidvaish97@gmail.com>
2025-12-08 13:46:26 +0530

    Add Partner mapper

---
+++ fhir-odoo-mapper/src/main/java/com/ozonehis/fhir/odoo/OdooUtils.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2024, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.fhir.odoo;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OdooUtils {
+
+    public static String convertEEEMMMddDateToOdooFormat(String date) {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy");
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        try {
+            LocalDate localDate = LocalDate.parse(date, inputFormatter);
+            return localDate.format(outputFormatter);
+        } catch (DateTimeParseException e) {
+            log.error("Cannot convert input date to Odoo date. Error: {}", e.getMessage());
+            return ""; // Returning empty string if not able to parse input date
+        }
+    }
+}
